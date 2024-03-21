937ed6b16670666b125a2bb910c489c75e449a19
thetuyen0802 <thetuyen0802@gmail.com>
2024-03-21 15:44:25 +0700

    Hanhnph

---
+++ 3.DAL/IRepositories/IUserRepo.cs
@@ -0,0 +1,18 @@
+﻿using _3.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.DAL.IRepositories
+{
+    public  interface IUserRepo
+    {
+        bool Add(User nd);
+        bool Update(User nd);   
+        bool Delete(User nd);
+        User FindById(int id);
+        List<User> GetAll();
+    }
+}
---
+++ 3.DAL/Repositories/UserRepo.cs
@@ -0,0 +1,52 @@
+﻿using _3.DAL.Context;
+using _3.DAL.IRepositories;
+using _3.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.DAL.Repositories
+{
+    public class UserRepo : IUserRepo
+    {
+        private DBContext _context;
+
+        public UserRepo()
+        {
+            _context = new DBContext(); 
+        }
+
+        public bool Add(User nd)
+        {
+            _context.Add(nd);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool Delete(User nd)
+        {
+            _context.Remove(nd);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public User FindById(int id)
+        {
+            return _context.Users.Find(id);
+        }
+
+        public List<User> GetAll()
+        {
+           return _context.Users.ToList();
+        }
+
+        public bool Update(User nd)
+        {
+            _context.Update(nd);
+            _context.SaveChanges();
+            return true;
+        }
+    }
+}
